31d0e0dcffd678644b6173fc21b987e2d74b5036
Alvin Phan <alvinphantastic@gmail.com>
2023-06-01 15:26:38 -0700

    Started memberList
    Added memberList which takes care of naming service when adding a node.  It's obviously not finished. Use go get github.com/hashicorp/memberlist

--- main.go
+++ main.go
@@ -1,22 +1,47 @@
 package main
-import ("github.com/gin-gonic/gin"
-		"github.com/jmoiron/sqlx"
-		_ "github.com/lib/pq"
-		"log"
-		)
-const	(db1ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db1 sslmode=disable"
-		db2ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db2 sslmode=disable"
-		)
 
-func main(){
+import (
+	"log"
+
+	"github.com/gin-gonic/gin"
+	"github.com/hashicorp/memberlist"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
+)
+
+const (
+	db1ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db1 sslmode=disable"
+	db2ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db2 sslmode=disable"
+)
+
+func main() {
+	log.Println("Starting node.")
+
+	config := memberlist.DefaultLocalConfig()
+	list, err := memberlist.Create(config)
+	if err != nil {
+		log.Println("Failed to create memberlist: " + err.Error())
+	}
+
+	clusterCount, err := list.Join([]string{"localhost"})
+	log.Println("Joing cluster of size", string(clusterCount))
+	if err != nil {
+		log.Println("Failed to join cluster: " + err.Error())
+	}
+
+	log.Println("Members:")
+	for _, member := range list.Members() {
+		log.Printf("  %s %s\n", member.Name, member.Addr)
+	}
+
 	db1, err := sqlx.Connect("postgres", db1ConnectionString)
-	if err != nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 	defer db1.Close()
 
 	db2, err := sqlx.Connect("postgres", db2ConnectionString)
-	if err != nil{
+	if err != nil {
 		log.Fatalln(err)
 	}
 	defer db2.Close()
@@ -25,7 +50,7 @@ func main(){
 	participant1 := &Participant{DB: db1}
 	participant2 := &Participant{DB: db2}
 	router := gin.Default()
-	
+
 	router.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
 			"message": "pong",
