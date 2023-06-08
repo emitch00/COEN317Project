88ed7c0978482b40e583e8e674260f3494e43b23
emitch00 <70246125+emitch00@users.noreply.github.com>
2023-06-08 03:37:00 -0700

    added more functionality to transactions, checks for password and valid users

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ import ("github.com/gin-gonic/gin"
 		"github.com/jmoiron/sqlx"
 		_ "github.com/lib/pq"
 		"log"
+		"golang.org/x/sync/errgroup"
 		)
 const	(db1ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db1 sslmode=disable"
 		db2ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db2 sslmode=disable"
+		db3ConnectionString = "host=127.0.0.1 port=1433 user=postgres password=password dbname=db3 sslmode=disable"
 		)
+
+var		(g errgroup.Group)
 
 func main(){
 	db1, err := sqlx.Connect("postgres", db1ConnectionString)
@@ -21,9 +25,16 @@ func main(){
 	}
 	defer db2.Close()
 
+	db3, err := sqlx.Connect("postgres", db2ConnectionString)
+	if err != nil{
+		log.Fatalln(err)
+	}
+	defer db3.Close()
+
 	coordinator := &Coordinator{DB: db1}
-	participant1 := &Participant{DB: db1}
-	participant2 := &Participant{DB: db2}
+	participant1 := &Participant{DB: db1, ID:1, Password:"one"}
+	participant2 := &Participant{DB: db2, ID:2, Password:"two"}
+	participant3 := &Participant{DB: db3, ID:3, Password:"three"}
 	router := gin.Default()
 	
 	router.GET("/ping", func(c *gin.Context) {
@@ -32,7 +43,7 @@ func main(){
 		})
 	})
 
-	register(router, coordinator, []*Participant{participant1, participant2})
+	register(router, coordinator, []*Participant{participant1, participant2, participant3})
 
 	router.Run(":8080")
 }
